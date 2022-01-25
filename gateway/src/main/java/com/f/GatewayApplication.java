905e71e97ffe131509571eb06d1abd31ae84ad10
liufeng <liuf@sunwayworld.com>
2022-01-25 14:56:16 +0800

    多级模块构建

---
+++ gateway/src/main/java/com/f/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ *
+ * @author liuf
+ * @date 2022/1/25 11:37
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
